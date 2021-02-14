a8855badc0fa708add70339c3f5dea7131786917
Aayush Mishra <aayush.mishra2019@vitbhopal.ac.in>
2021-02-14 22:09:08 +0530

    Add files via upload

---
+++ QR Code Generator.py
@@ -0,0 +1,32 @@
+import pyqrcode as df
+from tkinter import *
+from tkinter.filedialog import *
+
+def generate():       #genrating QR code
+    qr=entry.get()
+    destination = askdirectory()
+    filename= filename = asksaveasfilename(filetypes=[('PNG', '.png'),('SVG','.svg')
+   ,('All files', '*')], defaultextension='.png')
+    a=df.create(qr)
+    a.png(filename)
+#For GUI
+root= Tk()
+root.geometry('400x160')
+root.title("QR Code Generator")
+
+label = Label(root,text="Enter URL or Text",font=10).pack(fill=X,padx=15)
+entry=Entry(root,width=100,font=10)
+entry.pack(padx=15)
+Button(root,text="Generate",command=generate,relief=RAISED,width=15,font=10).pack(pady=5)
+
+root.mainloop()
+
+
+
+
+
+
+
+
+
+
